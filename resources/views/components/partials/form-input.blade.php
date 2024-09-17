04a64aac20bc09977b87873485b528d85d756616
Wrseno <warsenosetyono@gmail.com>
2024-09-17 07:45:03 +0700

    feat: add form input components

---
+++ resources/views/components/partials/form-input.blade.php
@@ -0,0 +1,16 @@
+@props([ 'type' => 'text', 'name', 'placeholder', 'label' ])
+
+<label for="{{ $name }}" class="block text-sm font-medium text-gray-800">
+    {{ $label }}
+</label>
+<input
+    type="{{ $type }}"
+    id="{{ $name }}"
+    name="{{ $name }}"
+    required
+    placeholder="{{ $placeholder }}"
+    {{ $attributes->merge([ 'class'=>"mt-1 block w-full px-3 py-2 border border-gray-300
+    rounded-md shadow-sm focus:outline-none focus:ring-co-dark-blue
+    focus:border-co-dark-blue" 
+    ]) }} 
+/>
