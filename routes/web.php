844e23ed2ccbd2afba6f6ba0927be3afaee70687
Wrseno <warsenosetyono@gmail.com>
2024-09-17 19:55:43 +0700

    faet: add view controller

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\CompetitionController;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
 
-Route::get('/', function () {
-    return ['Laravel' => app()->version()];
+
+Route::middleware('auth')->group(function () {
 });
 
-Route::middleware('auth')->group(function () {});
+Route::get('/', [HomeController::class, 'index']);
+Route::get('/register', [RegisterController::class, 'index']);
+Route::get('/login', [AuthenticatedSessionController::class, 'index']);
+Route::prefix('/competition')->controller(CompetitionController::class)->group(function() {
+    Route::get('/web-app', 'webApp');
+});
