8a24977ec6146a350c8327576ed0116ce6dff7dd
Wrseno <warsenosetyono@gmail.com>
2024-09-19 14:17:14 +0700

    feat: add select option

--- resources/views/pages/auth/register.blade.php
+++ resources/views/pages/auth/register.blade.php
@@ -1,17 +1,20 @@
 @extends('layouts.auth') @section('title', 'Techomfest 2025 Register Form')
 
 <div class="mt-8 px-12">
-    <x-partials.button-link href="/" class="w-[150px] flex items-center gap-2 bg-slate-100">
+    <x-partials.button-link
+        href="/"
+        class="w-[150px] flex items-center gap-2 bg-slate-100"
+    >
         <x-partials.back-svg />
         Kembali
     </x-partials.button-link>
 </div>
 <div class="px-4 md:px-12 mx-auto">
     <div class="grid md:grid-cols-2 gap-8">
-        <div class="">
+        <div class="grid h-full items-center">
             <img
                 src="{{ asset('img/tcfest 2025.png') }}"
-                alt=""
+                alt="Logo Techomfest 2025"
                 class="w-full"
             />
         </div>
@@ -22,25 +25,20 @@
             </div>
             <form action="" method="post">
                 <div class="my-4">
-                    <div class="my-4">
                     <x-partials.form-input
                         label="Nama"
                         name="name"
-                        placeholder="Masukkan nama"
+                        placeholder="Masukkan nama lengkap"
                     />
                 </div>
-                </div>
                 <div class="my-4">
-                    <div class="my-4">
                     <x-partials.form-input
                         label="Email"
                         name="email"
-                        placeholder="Masukkan Email"
+                        placeholder="Masukkan email"
                     />
                 </div>
-                </div>
                 <div class="my-4">
-                    <div class="my-4">
                     <x-partials.form-input
                         label="Password"
                         name="password"
@@ -48,6 +46,42 @@
                         type="password"
                     />
                 </div>
+                <div class="my-4">
+                    <x-partials.form-input
+                        label="Confirm Password"
+                        name="confirm_password"
+                        placeholder="Masukkan konfirmasi password"
+                        type="password"
+                    />
+                </div>
+                <div class="my-4">
+                    <x-partials.form-input
+                        label="Nomor HP"
+                        name="no_hp"
+                        placeholder="Masukkan konfirmasi password"
+                        type="text"
+                    />
+                </div>
+                <div class="my-4">
+                    <x-partials.form-select
+                        name="gender_id"
+                        labelName="Jenis Kelamin"
+                    >
+                        <option disabled selected>Pilih Jenis Kelamin</option>
+                        <option value="L">Laki-Laki</option>
+                        <option value="P">Perempuan</option>
+                    </x-partials.form-select>
+                </div>
+                <div class="my-4">
+                    <x-partials.form-select
+                        name="kategori_id"
+                        labelName="Kategori Lomba"
+                    >
+                        <option selected disabled>Pilih Kategori Lomba</option>
+                        <option value="L">Web App</option>
+                        <option value="P">Capture the Flag</option>
+                        <option value="P">UI/UX Design</option>
+                    </x-partials.form-select>
                 </div>
                 <button
                     class="w-full mt-4 mb-12 p-2 px-6 bg-gradient-to-r from-[#263B81] to-[#DB0056] text-white rounded-md"
