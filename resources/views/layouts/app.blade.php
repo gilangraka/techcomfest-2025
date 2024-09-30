93907fdfe6380f5bfa9ba11128aef8bd437f323f
Gilang Raka Ramadhan <gilangrakar@gmail.com>
2024-09-30 15:24:40 +0700

    Add logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,26 +1,30 @@
 <!DOCTYPE html>
 <html lang='{{ str_replace('_', '-', app()->getLocale()) }}'>
-    <head>
-        <meta charset='utf-8'>
-        <meta name='viewport' content='width=device-width, initial-scale=1'>
 
-        <title>@yield('title')</title>
+<head>
+    <meta charset='utf-8'>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
 
-        @vite('resources/css/app.css')
-        <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-    </head>
-    <body class="text-slate-800 flex flex-col gap-y-32">
+    <title>@yield('title')</title>
+    <link rel="icon" href="{{ asset('img/tcfest%202025.png') }}" type="image/png">
+
+    @vite('resources/css/app.css')
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+</head>
+
+<body class="text-slate-800 flex flex-col gap-y-32">
 
     @include('components.header')
 
-        <main class='container mx-auto flex flex-col gap-y-32'>
-            @yield('content')
-        </main>
+    <main class='container mx-auto flex flex-col gap-y-32'>
+        @yield('content')
+    </main>
 
     @include('components.footer')
 
     <script src='https://unpkg.com/ionicons@4.5.10-0/dist/ionicons.js'></script>
     @yield('scripts')
 
-    </body>
+</body>
+
 </html>
--- resources/views/layouts/dashboard/layout.blade.php
+++ resources/views/layouts/dashboard/layout.blade.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Techcomfest 2025 | @yield('title')</title>
+    <link rel="icon" href="{{ asset('img/tcfest%202025.png') }}" type="image/png">
+
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fontsource/source-sans-3@5.0.12/index.css"
         integrity="sha256-tXJfXfp6Ewt1ilPzLDtQnJV4hclT9XuaZUKyUvmyr+Q=" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/overlayscrollbars@2.3.0/styles/overlayscrollbars.min.css"
